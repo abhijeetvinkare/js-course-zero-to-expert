a6c3dd525a7ebec5f1912ef823d4240d64b54db1
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2023-12-02 01:07:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Challenge1 from "./Coding Challenge/Challenge1"
+
+function App() {
+
+  return (
+    <>
+      <Challenge1 />
+    </>
+  )
+}
+
+export default App
---
+++ src/Coding Challenge/Challenge1.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Challenge1() {
+  const markData = {
+    height: 1.69,
+    weight: 78,
+  };
+
+  const johnData = {
+    height: 1.95,
+    weight: 92,
+  };
+
+  const markBMI = markData.weight / (markData.height * markData.height);
+  const johnBMI = johnData.weight / (johnData.height * johnData.height);
+  const markHeigherBMI = markBMI > johnBMI;
+
+  console.log(markBMI, johnBMI, markHeigherBMI);
+
+  if (markHeigherBMI) {
+    console.log(`Mark's BMI (${(markBMI).toFixed(1)}) is higher than John's BMI (${(johnBMI).toFixed(1)})`);
+  }else{
+    console.log(`John's BMI (${(johnBMI).toFixed(1)}) is higher than Marks's BMI (${(markBMI).toFixed(1)})`);
+  }
+
+  return <div>Challenge1</div>;
+}
+
+export default Challenge1;
