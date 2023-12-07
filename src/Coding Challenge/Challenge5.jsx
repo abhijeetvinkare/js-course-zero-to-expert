6b43ee363f01e7913ff97ac9075bdb9438ebbd17
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2023-12-07 22:36:12 +0530

    initial commit

--- src/Coding Challenge/Challenge5.jsx
+++ src/Coding Challenge/Challenge5.jsx
@@ -18,10 +18,11 @@ HINT: Remember that an array needs a value in each position, and that value can 
 GOOD LUCK 😀
 */
 
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-}
-// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// }
+
+const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
 const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
