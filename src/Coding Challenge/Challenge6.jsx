0daaa7d72c5058a63913fba9d7b490cb6eca087d
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2023-12-08 18:24:02 +0530

    initial commit

--- src/Coding Challenge/Challenge6.jsx
+++ src/Coding Challenge/Challenge6.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Challenge6() {
-
   const jonas = {
-    firstName:"jonas",
-    lastName:"shew",
-    age:2037-1991,
-    job:"teacher",
-    friends:["michel", "peter", "steven"]
-  }
+    firstName: "jonas",
+    lastName: "shew",
+    age: 2037 - 1991,
+    job: "teacher",
+    friends: ["michel", "peter", "steven"],
+    hasDrivingli: true,
+    getSummery: function () {
+      console.log(
+        `${this.firstName} is a ${this.age}-year old ${this.job}, and he has ${
+          this.hasDrivingli ? "a" : "no"
+        } driving license`
+      );
+    },
+  };
 
-  console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is ${jonas.friends[0]}`)
+  console.log(
+    `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is ${jonas.friends[0]}`
+  );
 
-  return (
-    <div>Challenge6</div>
-  )
+  jonas.getSummery();
+
+  return <div>Challenge6</div>;
 }
 
-export default Challenge6
+export default Challenge6;
