33418db6d306e08a6ff0f60b0b9dd382a11306a7
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2023-12-08 17:34:44 +0530

    initial commit

--- src/Coding Challenge/Challenge5.jsx
+++ src/Coding Challenge/Challenge5.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 function Challenge5() {
-    // Coding Challenge #2
+  // Coding Challenge #2
 
-/*
+  /*
 Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
 
 1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
@@ -18,21 +18,16 @@ HINT: Remember that an array needs a value in each position, and that value can 
 GOOD LUCK 😀
 */
 
-// const calcTip = function (bill) {
-//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-// }
+  const bills = [125, 555, 44];
 
-const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  const calcTip = (billAmount) => billAmount >= 50 && billAmount <= 300 ? billAmount * (15 / 100) : billAmount * (20 / 100);
 
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+  const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+  const totals = [bills[0]+tips[0], bills[1]+tips[1], bills[2]+tips[2]]
 
-console.log(bills, tips, totals);
+  console.log(bills, tips, totals);
 
-  return (
-    <div>Challenge5</div>
-  )
+  return <div>Challenge5</div>;
 }
 
-export default Challenge5
+export default Challenge5;
