f743e506b3e9f82f6a574aa04e60b09e0d5c1d84
karan vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2023-12-19 17:28:27 +0530

    initial commit

--- src/Coding Challenge/Challenge8.jsx
+++ src/Coding Challenge/Challenge8.jsx
@@ -185,15 +185,27 @@ function Challenge8() {
 // isIsogram "moose" = false
 // isIsogram "aba" = false
 
-function isIsogram(str){
-  const letters = str.toLowerCase().split('');
-  return letters.every((letter, index) => letters.indexOf(letter) === index);
-}
+// function isIsogram(str){
+//   const letters = str.toLowerCase().split('');
+//   return letters.every((letter, index) => letters.indexOf(letter) === index);
+// }
 
-console.log(isIsogram("Hello"))
-console.log(isIsogram("Dermatoglyphics"))
-console.log(isIsogram("moose"))
-console.log(isIsogram("aba"))
+// console.log(isIsogram("Hello"))
+// console.log(isIsogram("Dermatoglyphics"))
+// console.log(isIsogram("moose"))
+// console.log(isIsogram("aba"))
+
+
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
 
   return <div>Challenge8</div>;
 }
