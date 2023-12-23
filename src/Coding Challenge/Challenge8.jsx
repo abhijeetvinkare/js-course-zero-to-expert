d1d159744194bb14507dd3ce48d8ed83115fa208
Abhijeet Vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2023-12-23 22:25:11 +0530

    Update Challenge8.jsx

--- src/Coding Challenge/Challenge8.jsx
+++ src/Coding Challenge/Challenge8.jsx
@@ -229,6 +229,14 @@ function sortStringWithNumbers(inputString) {
   console.log(sortStringWithNumbers("are2 you3 how1"));
   console.log(sortStringWithNumbers("are you how"))
 
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples:
+
+// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+// spinWords( "This is a test") => returns "This is a test" 
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+
   return <div>Challenge8</div>;
 }
 
