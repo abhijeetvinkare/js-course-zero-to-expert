c48fcf14666c94c4479588762b2d39a16f964377
Abhijeet Vinkare <97100940+abhijeetvinkare@users.noreply.github.com>
2024-02-02 20:54:09 +0530

    Update Challenge8.jsx

--- src/Coding Challenge/Challenge8.jsx
+++ src/Coding Challenge/Challenge8.jsx
@@ -275,6 +275,25 @@ function Challenge8() {
 // console.log(nbYear(1500, 5, 100, 5000));  // Output: 15
 // console.log(nbYear(1500000, 2.5, 10000, 2000000));  // Output: 10
 
+// Write a function that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zero-ith index is even, therefore that character should be upper cased and you need to start over for each word.
+
+// The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+
+// Examples:
+// "String" => "StRiNg"
+// "Weird string case" => "WeIrD StRiNg CaSe"
+
+
+function toWeirdCase(str){
+  return str.split(' ').map(word => {
+    return word.split('').map((char, index) => {
+      return index % 2 === 0 ? char.toUpperCase() : char.toLowerCase();
+    }).join('');
+  }).join(' ');
+}
+
+console.log(toWeirdCase("String"))
+
   return <div>Challenge8</div>;
 }
 
